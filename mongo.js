c0b1f5e33bcf574c6a4332bac9002a8ede8f1f9a
Safdar <71539665+malta-khan@users.noreply.github.com>
2023-06-13 12:00:46 +0500

    update mongo file

--- mongo.js
+++ mongo.js
@@ -1,31 +1,29 @@
-const mongoose = require('mongoose')
-
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+   console.log('give password as argument')
+   process.exit(1)
 }
 
+const mongoose = require('mongoose')
+mongoose.pluralize(null);
 const password = process.argv[2]
-
-const url =
-  `mongodb+srv://wipetmpf3wal:${password}@cluster0.uwvfkir.mongodb.net/?retryWrites=true&w=majority`
-
-mongoose.set('strictQuery',false)
+const url = `mongodb+srv://wipetmpf3wal:${password}@cluster0.uwvfkir.mongodb.net/phoneBookTest?retryWrites=true&w=majority`
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+   name: String,
+   phone: Number
 })
+const Person = mongoose.model("PersonsTest", personSchema);
+if (process.argv.length === 3) {
+   Person.find().then(result=>{
+      console.log(result);
+      mongoose.connection.close();
+   })
+} else {
+   const newContact = new Person({ name: process.argv[3], phone: process.argv[4] })
+   newContact.save().then(result => {
+      console.log(`saved ${result.name} (${result.phone}) to database`)
+      mongoose.connection.close()
+   })
+}
 
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'HTML is hard',
-  important: false,
-})
-
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
