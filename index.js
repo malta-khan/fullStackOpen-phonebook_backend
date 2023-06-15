c07080bb9106a3a4fc5d5191002df372f048a287
Safdar <71539665+malta-khan@users.noreply.github.com>
2023-06-15 13:14:28 +0500

    fixed style with eslint

--- index.js
+++ index.js
@@ -1,94 +1,92 @@
-const express = require("express")
-const morgan = require("morgan")
-const uniqid = require("uniqid")
-const app = express()
-const cors = require('cors')
-const PORT = process.env.PORT || 3001
-const mongoose = require('mongoose')
-const Person = require("./models/person")
-mongoose.set('strictQuery', false)
-const url = process.env.MONGODB_URI
+const express = require('express');
+const morgan = require('morgan');
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('frontend'))
+const app = express();
+const cors = require('cors');
 
-morgan.token('jsonData', (req, res) => { return JSON.stringify(req.body) })
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :jsonData"))
+const PORT = process.env.PORT || 3001;
+const mongoose = require('mongoose');
+const Person = require('./models/person');
 
+mongoose.set('strictQuery', false);
+const url = process.env.MONGODB_URI;
 
-app.get("/api/persons", (request, response) => {
-    Person.find().then((result) => {
-        response.send(result);
-    })
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.static('frontend'));
 
-app.post("/api/persons", (request, response, next) => {
-    let newPerson = new Person({ ...request.body });
-    newPerson.save()
+morgan.token('jsonData', (request) => JSON.stringify(request.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :jsonData'));
+
+app.get('/api/persons', (request, response) => {
+  Person.find().then((result) => {
+    response.send(result);
+  });
+});
+
+app.post('/api/persons', (request, response, next) => {
+  const newPerson = new Person({ ...request.body });
+  newPerson.save()
     .then((result) => {
-        response.send(result)
+      response.send(result);
     })
-    .catch(err=>{
-        next(err)
-    })
-})
+    .catch((err) => {
+      next(err);
+    });
+});
 
-app.get("/api/persons/:id", (request, response, next) => {
-    let { id } = request.params;
-    Person.findById(id).then((result) => {
-        if (result) {
-            response.send(result)
-        } else {
-            response.status(404).end("404 Invalid ID")
-        }
-    })
-        .catch(err => next(err))
-})
+app.get('/api/persons/:id', (request, response, next) => {
+  const { id } = request.params;
+  Person.findById(id).then((result) => {
+    if (result) {
+      response.send(result);
+    } else {
+      response.status(404).end('404 Invalid ID');
+    }
+  })
+    .catch((err) => next(err));
+});
 
-app.put("/api/persons/:id", (request, response,next) => {
-    let { id } = request.params;
-    Person.findByIdAndUpdate(id, request.body, { new: true , runValidators: true})
-        .then((result) => response.send(result))
-        .catch(error=>{
-            next(error)
-        })
-})
-app.delete("/api/persons/:id", (request, response) => {
-    let { id } = request.params;
-    Person.deleteOne({ _id: id }).then((result) => {
-        console.log(result)
-        if(result.deletedCount === 1){
-           response.status(204).end()
-        }else{
-            return response.status(400).send("contact does not exsist or already deleted.")
-        }
-    })
-})
+app.put('/api/persons/:id', (request, response, next) => {
+  const { id } = request.params;
+  Person.findByIdAndUpdate(id, request.body, { new: true, runValidators: true })
+    .then((result) => response.send(result))
+    .catch((error) => {
+      next(error);
+    });
+});
+app.delete('/api/persons/:id', (request, response) => {
+  const { id } = request.params;
+  Person.deleteOne({ _id: id }).then((result) => {
+    if (result.deletedCount === 1) {
+      return response.status(204).end();
+    }
+    return response.status(400).send('contact does not exsist or already deleted.');
+  });
+});
 
-app.get("/info", (request, response) => {
-    Person.find().then(result => {
-        let date = new Date()
-        let html = `
+app.get('/info', (request, response) => {
+  Person.find().then((result) => {
+    const date = new Date();
+    const html = `
         <div>Phonebook has info for ${result.length} people.</div>
-        <div>${date}</div>`
-        response.send(html)
-    })
-})
+        <div>${date}</div>`;
+    response.send(html);
+  });
+});
 
 function errorHandler(err, req, res, next) {
-    if (err.name === "CastError") {
-        return res.status(400).send("Invalid ID format")
-    } else if (err.name ==="ValidationError"){
-        return res.status(400).json({ error: err.message })
-    } else {
-        next(err)
-    }
+  if (err.name === 'CastError') {
+    return res.status(400).send('Invalid ID format');
+  } if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message });
+  }
+  return next(err);
 }
-app.use(errorHandler)
+app.use(errorHandler);
 
 mongoose.connect(url).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}/`)
-    })
-})
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}/`);
+  });
+});
